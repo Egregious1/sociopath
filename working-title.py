5f39a1b9bafd503300252dbae62d2be81b03a010
Brian Kahler <briankahler.designs@gmail.com>
2021-07-15 00:16:59 -0700

    Started building new game made intro player name input and some asthetics

---
+++ working-title.py
@@ -0,0 +1,38 @@
+#Attempt two at making a text based game. The first one was directionless and more for building foundation this one should prove better than the last.
+
+def titleScreen():
+    print('==============================')
+    print('=                            =')
+    print('=                            =')
+    print('=     Working title          =')
+    print('=                            =')
+    print('=                            =')
+    print('=                            =')
+    print('=                            =')
+    print('==============================')
+    print('\n' * 2)
+    playerName = input('What is your name? ')
+    print()
+    print(f'Welcome {playerName}!')
+
+def introStory():
+    print('     "You are always so full of shit!"')
+    print('\n' * 2)
+    print('               "Why are you doing this to us?"')
+    print('\n' * 2)
+    print('      "You only care about yourself!"')
+    print('\n' * 2)
+    print('"We are not friends anymore..."')
+    print('\n' * 6)
+
+
+
+
+
+
+titleScreen()
+print('============================')
+print('\n' * 6)
+introStory()
+print()
+
