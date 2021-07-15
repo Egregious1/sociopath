98673e1196f8b1004b7ff83b484d461a25d5085a
Brian Kahler <briankahler.designs@gmail.com>
2021-07-15 00:37:28 -0700

    added more to intro

--- working-title.py
+++ working-title.py
@@ -15,6 +15,7 @@ def titleScreen():
     print()
     print(f'Welcome {playerName}!')
 
+# start of game. always the same when starting game.
 def introStory():
     print('     "You are always so full of shit!"')
     print('\n' * 2)
@@ -33,10 +34,36 @@ def introStory():
     input('press enter')
     print('\n' * 6)
     print('Its half past noon and you have been asleep for what feels like days.')
+    print(' ')
     print('you wake up to a dark room that smells of metal, mold, chemicals,')
+    print(' ')
     print(' and a faint smell of rotting food')
     print('\n' * 2)
     input('press enter')
+    print('\n' * 6) 
+    print('You ask yourself "Why do I keep living like this?"')
+    print(' ')
+    print('Immediatley you forget about that feeling...')
+    print(' ')
+    print('Carefully you move through the room past all the tools and bottles')
+    print(' ')
+    print('You flip the light switch to the bathroom')
+    print(' ')
+    print('                    Click!')
+    input('press enter')
+
+# using old code from frist game to make this simple path input.
+def choosePath():
+    path = input('What do you choose? [L or R] ')
+    if path == 'L' or path == 'l':
+        print('predictable')
+    elif path == 'R' or path == 'r':
+        print('Afraid of change?')
+    else:
+        if path != 'l' or path != 'L' or path != 'r' or path != 'R':
+            print('try again')
+            print(' ')
+            choosePath()
 
 
 
